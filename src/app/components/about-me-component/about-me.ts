5cb062bf96c1641ee3a07b0d5eb79f48dc0f2621
Javier <schjavier@gmail.com>
2026-01-02 06:45:21 -0300

    add: link too home page, and some styles to about-me text

--- src/app/components/about-me-component/about-me.ts
+++ src/app/components/about-me-component/about-me.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-about-me',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterLink
   ],
   templateUrl: './about-me.html',
   styleUrl: './about-me.css',
