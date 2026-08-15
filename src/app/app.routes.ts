f52889576a90eea082d80c1d9c9668114221d21c
Javier <schjavier@gmail.com>
2026-08-15 11:11:23 -0300

    feat(experience): add experience component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {AboutMe} from './components/about-me-component/about-me';
 import {ProjectComponent} from './components/project-component/project-component';
 import {EducationComponent} from './components/education/education';
 import {ContactComponent} from './components/contact/contact';
+import {ExperienceComponent} from './components/experience/experience';
 
 export const routes: Routes = [
   {
@@ -31,6 +32,10 @@ export const routes: Routes = [
   {
     path: 'Contacto',
     component: ContactComponent
+  },
+  {
+    path: 'FullStack Developer',
+    component: ExperienceComponent
   },
   {
     path: ':projectName',
